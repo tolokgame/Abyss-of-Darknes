b5dc59d4af0ce77bb61677211c8d9c31cf10e025
tolokgame <khazhomiia2009sergiy@gmail.com>
2026-08-02 19:06:04 +0300

    AI test
    AI Test

---
+++ Assets/Scripts/TestOnly/ChangeColor.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+
+public class ChangeColor : MonoBehaviour
+
+    
+{
+
+    private MeshRenderer meshRenderer;
+    void Start()
+    {
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        Application.targetFrameRate = 10;
+
+        StartCoroutine(ChangeColor);
+    }
+
+    private IEnumerator ChangePosition()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.5f);
+        while (true)
+        {
+            yield return null;
+            
+        }
+    }
+
+}
