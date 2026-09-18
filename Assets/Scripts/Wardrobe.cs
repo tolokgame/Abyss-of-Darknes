de8ea4833569abd02409a6390f2ec7d758c4551f
tolokgame <khazhomiia2009sergiy@gmail.com>
2026-09-18 19:45:05 +0300

    Wardrobe visual fix
    Now camera works properly(Plase, God, save me, Im tired of fixining someone`s bugs)

--- Assets/Scripts/Wardrobe.cs
+++ Assets/Scripts/Wardrobe.cs
@@ -20,6 +20,144 @@ public class Wardrobe : MonoBehaviour
     private Vector3 previousPlayerPosition;
     private Quaternion previousPlayerRotation;
 
+    private Vector3 previousCameraPosition;
+    private Quaternion previousCameraRotation;
+
+    private void Update()
+    {
+        if (isPlayerInZone && Input.GetKeyDown(interactKey))
+        {
+            if (!isPlayerInside)
+                HidePlayer();
+            else
+                ExitWardrobe();
+        }
+    }
+
+    private void HidePlayer()
+    {
+        if (playerObject == null)
+            return;
+
+        playerController = playerObject.GetComponent<PlayerController>();
+        playerState = playerObject.GetComponent<PlayerState>();
+
+        isPlayerInside = true;
+
+        previousPlayerPosition = playerObject.transform.position;
+        previousPlayerRotation = playerObject.transform.rotation;
+
+        if (playerController != null && playerController.cameraHolder != null)
+        {
+            previousCameraPosition = playerController.cameraHolder.position;
+            previousCameraRotation = playerController.cameraHolder.rotation;
+        }
+
+        if (insidePoint != null)
+        {
+            playerObject.transform.position = insidePoint.position;
+            playerObject.transform.rotation = insidePoint.rotation;
+        }
+        else
+        {
+            playerObject.transform.position = transform.position;
+        }
+
+        if (cameraPoint != null && playerController != null)
+        {
+            playerController.cameraHolder.position = cameraPoint.position;
+            playerController.cameraHolder.rotation = cameraPoint.rotation;
+        }
+
+        if (playerState != null)
+        {
+            playerState.IsHidden = true;
+        }
+    }
+
+    private void ExitWardrobe()
+    {
+        if (playerObject == null)
+            return;
+
+        if (playerState != null)
+        {
+            playerState.IsHidden = false;
+        }
+
+        if (exitPoint != null)
+        {
+            playerObject.transform.position = exitPoint.position;
+            playerObject.transform.rotation = exitPoint.rotation;
+        }
+        else
+        {
+            playerObject.transform.position = previousPlayerPosition;
+            playerObject.transform.rotation = previousPlayerRotation;
+        }
+
+        if (playerController != null && playerController.cameraHolder != null)
+        {
+            playerController.cameraHolder.position = previousCameraPosition;
+            playerController.cameraHolder.rotation = previousCameraRotation;
+        }
+
+        isPlayerInside = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        isPlayerInZone = true;
+        playerObject = other.gameObject;
+
+        playerController = playerObject.GetComponent<PlayerController>();
+        playerState = playerObject.GetComponent<PlayerState>();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        isPlayerInZone = false;
+
+        if (!isPlayerInside)
+        {
+            playerObject = null;
+            playerController = null;
+            playerState = null;
+        }
+    }
+}
+
+
+
+
+/*using UnityEngine;
+
+public class Wardrobe : MonoBehaviour
+{
+    [Header("Interaction")]
+    [SerializeField] private KeyCode interactKey = KeyCode.E;
+
+    [Header("Positions")]
+    [SerializeField] private Transform insidePoint;
+    [SerializeField] private Transform cameraPoint;
+    [SerializeField] private Transform exitPoint;
+
+    private bool isPlayerInside = false;
+    private bool isPlayerInZone = false;
+
+    private GameObject playerObject;
+    private PlayerController playerController;
+    private PlayerState playerState;
+
+    private Vector3 previousPlayerPosition;
+    private Quaternion previousPlayerRotation;
+
     private void Update()
     {
         if (isPlayerInZone && Input.GetKeyDown(interactKey))
@@ -70,6 +208,8 @@ public class Wardrobe : MonoBehaviour
         }
     }
 
+
+
     private void ExitWardrobe()
     {
         if (playerObject == null)
@@ -95,6 +235,8 @@ public class Wardrobe : MonoBehaviour
 
         isPlayerInside = false;
     }
+    
+
 
     private void OnTriggerEnter(Collider other)
     {
